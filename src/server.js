a436c290ba8096e9e327b79b0f66bca1c026d020
Hunter Trujillo <cryptoquick@pm.me>
2019-05-05 14:22:08 -0600

    Fix production server port.

--- src/server.js
+++ src/server.js
@@ -12,6 +12,6 @@ polka() // You can also use Express
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
-	.listen(PORT, err => {
+	.listen(NODE_ENV === 'production' ? 80 : PORT, err => {
 		if (err) console.log('error', err);
 	});
