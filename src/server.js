8ececd00de152aca7e773090c88ddaa1e32f5d2d
Hunter Trujillo <cryptoquick@pm.me>
2019-05-10 04:11:50 -0600

    Return server to running only on port 3000, even in production. Now going to run with Nginx.

--- src/server.js
+++ src/server.js
@@ -12,6 +12,6 @@ polka() // You can also use Express
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
-	.listen(NODE_ENV === 'production' ? 80 : PORT, err => {
+	.listen(PORT, err => {
 		if (err) console.log('error', err);
 	});
