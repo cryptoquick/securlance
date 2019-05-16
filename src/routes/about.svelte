59ab2d72f1d820134cd930d0dd03c52789bd1007
Hunter Trujillo <cryptoquick@pm.me>
2019-05-15 21:16:25 -0600

    Add server GA. Minor about copy change.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -12,8 +12,8 @@
 <h1>SecurLance</h1>
 
 <div class="info">
-	<p>Create secure invoices that showcase only random portions of work to a client as a sample.</p>
-	<p>Neither the freelancer nor the client get to choose which parts are shown; they are completely random.</p>
+	<p>Create secure invoices that showcase only random portions of work to a customer as a sample.</p>
+	<p>Neither the freelancer nor the customer get to choose which parts are shown; they are completely random.</p>
 	<p>Clients can verify the work has been done, but will need to pay in order to receive the full devliverable.</p>
 	<p>Works on PDFs, images, and code. Works on single files, entire folders, or zips.</p>
 	<p>Invoices can be paid with credit, deposit, or cryptocurrency.</p>
--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import sirv from 'sirv';
-import polka from 'polka';
+import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import expressGa from 'express-ga-middleware';
+
+const app = express()
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
+app
+	.use(expressGa('UA-42266637-2'))
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
